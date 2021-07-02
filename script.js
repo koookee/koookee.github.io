2d0bfe7babbb2c34c851209836655fd6c77c00ee
koookee <55768904+koookee@users.noreply.github.com>
2021-07-02 05:21:31 -0400

    Filtering by language now works

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 const swup = new Swup()
 const hasRefreshOnce = false;
-const filterIDs = ["AllFilter", "JavaScriptFilter", "C#Filter", "JavaFilter", "PythonFilter", "CFilter"];
+const filterIDs = ["All", "JavaScript", "C#", "Java", "Python", "C"];
 
 // Project filter by programming language
 function selectFilter(filterID){
   showFunc(filterID);
   hideFunc(filterID);
 }
-function showFunc(name){
-  document.getElementById(name).style.color = "#66FCF1";
-  /*
-  document.getElementById("hide").style.color = "#C5C6C7";
-  document.getElementById("test1").style.visibility = "visible";
-  document.getElementById("test1").style.opacity = 1;
-  document.getElementById("test1").style.transition = "opacity 1s linear";
-  */
-  /*
 
-  */
+function showFunc(name){
+  document.getElementById(name).style.color = "#66FCF1"; // Changes color of other sub navbar elements
+  if (name.localeCompare("All") == 0) {
+    for (var i = 0; i < filterIDs.length; i++) {    // Makes every card visible if filter is "All"
+      for (var j = 0; j < document.getElementsByClassName(filterIDs[i]).length; j++) {
+        document.getElementsByClassName(filterIDs[i])[j].style.visibility = "visible";
+        document.getElementsByClassName(filterIDs[i])[j].style.opacity = 1;
+        document.getElementsByClassName(filterIDs[i])[j].style.transition = "opacity 1s linear"
+        document.getElementsByClassName(filterIDs[i])[j].style.pointerEvents = "all";
+      }
+    }
+  }
+  for (var i = 0; i < document.getElementsByClassName(name).length; i++) {
+    document.getElementsByClassName(name)[i].style.visibility = "visible";
+    document.getElementsByClassName(name)[i].style.opacity = 1;
+    document.getElementsByClassName(name)[i].style.transition = "opacity 1s linear"
+    document.getElementsByClassName(name)[i].style.pointerEvents = "all";
+  }
 }
 
 function hideFunc(name){
-  filterIDs.forEach((item) => {
+  filterIDs.forEach((item) => {       // Changes color of other sub navbar elements
     if(item.localeCompare(name) != 0) document.getElementById(item).style.color = "#C5C6C7";
   });
-  /*
-  document.getElementById("hide").style.color = "#66FCF1";
-  document.getElementById("show").style.color = "#C5C6C7";
-  document.getElementById("test1").style.visibility = "hidden";
-  document.getElementById("test1").style.opacity = 0.01;
-  document.getElementById("test1").style.transition = "visibility 0s 1s, opacity 1s linear";
-  */
+  for (var i = 0; i < filterIDs.length; i++) {     // Filters the cards by the programming language
+    for (var j = 0; j < document.getElementsByClassName(filterIDs[i]).length && filterIDs[i] != name && name.localeCompare("All") != 0; j++) {
+      //document.getElementsByClassName(filterIDs[i])[j].style.visibility = "hidden";
+      document.getElementsByClassName(filterIDs[i])[j].style.pointerEvents = "none";
+      document.getElementsByClassName(filterIDs[i])[j].style.opacity = 0;
+      document.getElementsByClassName(filterIDs[i])[j].style.transition = "visibility 0s 1s, opacity 1s linear";
+    }
+  }
 }
-
-
 
 // Project pop ups
 function togglePopup(){
